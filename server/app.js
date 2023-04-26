2a76aa12df4881c68844097579fc7c517778ab8c
briannkhata <briannkhata@gmail.com>
2023-04-26 20:12:12 +0200

    house keeping

--- server/app.js
+++ server/app.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const homeRouter = require("./routes/home.js");
 const path = require("path");
-
+const hbs = require("hbs");
 const carRouter = require("./routes/car.js");
-
 const app = express();
 
 const PORT = process.env.PORT || 5002;
 
-const publicDirectory = path.join(__dirname, "./public/");
-app.use(express.static(publicDirectory));
+hbs.registerPartials(path.join(__dirname, "views/partials"), (err) => {});
+app.use(express.static(path.join(__dirname, "./public/")));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
--- server/controllers/HomeController.js
+++ server/controllers/HomeController.js
@@ -1,17 +1,17 @@
 const home = (req, res) => {
-  res.render("home");
+  res.render("home", { pageTitle: "Home" });
 };
 const about = (req, res) => {
-  res.render("about");
+  res.render("about", { pageTitle: "About Us" });
 };
 const contact = (req, res) => {
-  res.render("contact");
+  res.render("contact", { pageTitle: "Contact Us" });
 };
 const join = (req, res) => {
-  res.render("join");
+  res.render("join", { pageTitle: "Join" });
 };
 const login = (req, res) => {
-  res.render("login");
+  res.render("login", { pageTitle: "Login" });
 };
 
 const addMessage = (req, res) => {
