348d425bc569e0caebba32bb86a8c04070514b6a
briannkhata <briannkhata@gmail.com>
2023-05-01 15:03:29 +0200

    hkeeping

--- front/index.js
+++ front/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5003;
 
+app.use(express.static(__dirname + "/public"));
 app.set("views", "./views"); // Set the views directory
 app.set("view engine", "ejs");
 app.use("/", require("./routes/home"));
