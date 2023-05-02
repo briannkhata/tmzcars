d0a7b3daa87bee2e53160891f7f3200d43933bf0
Brian Nkhata <bnkhata@mra.mw>
2023-05-02 07:33:32 +0200

    added session library

--- front/index.js
+++ front/index.js
@@ -2,13 +2,21 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5003;
 const bodyParser = require("body-parser");
+const session = require("express-session");
 const flash = require("express-flash");
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-//app.use(flash());
+app.use(
+  session({
+    secret: "codeforgeek",
+    saveUninitialized: true,
+    resave: true,
+  })
+);
+
+app.use(flash());
 app.use(express.static(__dirname + "/public"));
-app.set("views", "./views"); // Set the views directory
+app.set("views", "./views");
 app.set("view engine", "ejs");
 app.use("/", require("./routes/home"));
 
